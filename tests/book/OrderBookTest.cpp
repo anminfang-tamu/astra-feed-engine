b1306db5e1e7751849f8200e6f7033e356c12a78
anminfang <anminfang@tamu.edu>
2026-05-08 14:55:01 -0500

    added bookupdate test case

--- tests/book/OrderBookTest.cpp
+++ tests/book/OrderBookTest.cpp
@@ -34,14 +34,14 @@ TEST(OrderBookTest, StartsWithEmptyTopOfBook) {
 TEST(OrderBookTest, AddOrdersUpdatesTopOfBook) {
   OrderBook book(42);
 
-  book.addOrder(makeMessage(1, 42, 10, 100, OrderSide::Buy,
-                            MessageType::AddOrder));
-  book.addOrder(makeMessage(2, 42, 11, 50, OrderSide::Buy,
-                            MessageType::AddOrder));
-  book.addOrder(makeMessage(3, 42, 13, 75, OrderSide::Sell,
-                            MessageType::AddOrder));
-  book.addOrder(makeMessage(4, 42, 12, 25, OrderSide::Sell,
-                            MessageType::AddOrder));
+  book.addOrder(
+      makeMessage(1, 42, 10, 100, OrderSide::Buy, MessageType::AddOrder));
+  book.addOrder(
+      makeMessage(2, 42, 11, 50, OrderSide::Buy, MessageType::AddOrder));
+  book.addOrder(
+      makeMessage(3, 42, 13, 75, OrderSide::Sell, MessageType::AddOrder));
+  book.addOrder(
+      makeMessage(4, 42, 12, 25, OrderSide::Sell, MessageType::AddOrder));
 
   const TopOfBook top = book.getTopOfBook();
 
@@ -54,10 +54,10 @@ TEST(OrderBookTest, AddOrdersUpdatesTopOfBook) {
 TEST(OrderBookTest, ModifyOrderUpdatesBestLevelQuantity) {
   OrderBook book(42);
 
-  book.addOrder(makeMessage(1, 42, 10, 100, OrderSide::Buy,
-                            MessageType::AddOrder));
-  book.modifyOrder(makeMessage(1, 42, 10, 40, OrderSide::Buy,
-                               MessageType::ModifyOrder));
+  book.addOrder(
+      makeMessage(1, 42, 10, 100, OrderSide::Buy, MessageType::AddOrder));
+  book.modifyOrder(
+      makeMessage(1, 42, 10, 40, OrderSide::Buy, MessageType::ModifyOrder));
 
   const TopOfBook top = book.getTopOfBook();
 
@@ -68,13 +68,13 @@ TEST(OrderBookTest, ModifyOrderUpdatesBestLevelQuantity) {
 TEST(OrderBookTest, DeleteOrderFallsBackToNextBestBid) {
   OrderBook book(42);
 
-  book.addOrder(makeMessage(1, 42, 10, 100, OrderSide::Buy,
-                            MessageType::AddOrder));
-  book.addOrder(makeMessage(2, 42, 11, 50, OrderSide::Buy,
-                            MessageType::AddOrder));
+  book.addOrder(
+      makeMessage(1, 42, 10, 100, OrderSide::Buy, MessageType::AddOrder));
+  book.addOrder(
+      makeMessage(2, 42, 11, 50, OrderSide::Buy, MessageType::AddOrder));
 
-  book.deleteOrder(makeMessage(2, 42, 0, 0, OrderSide::Buy,
-                               MessageType::DeleteOrder));
+  book.deleteOrder(
+      makeMessage(2, 42, 0, 0, OrderSide::Buy, MessageType::DeleteOrder));
 
   const TopOfBook top = book.getTopOfBook();
 
@@ -87,13 +87,13 @@ TEST(OrderBookTest, DeleteOrderFallsBackToNextBestBid) {
 TEST(OrderBookTest, DeleteOrderFallsBackToNextBestAsk) {
   OrderBook book(42);
 
-  book.addOrder(makeMessage(1, 42, 12, 100, OrderSide::Sell,
-                            MessageType::AddOrder));
-  book.addOrder(makeMessage(2, 42, 13, 50, OrderSide::Sell,
-                            MessageType::AddOrder));
+  book.addOrder(
+      makeMessage(1, 42, 12, 100, OrderSide::Sell, MessageType::AddOrder));
+  book.addOrder(
+      makeMessage(2, 42, 13, 50, OrderSide::Sell, MessageType::AddOrder));
 
-  book.deleteOrder(makeMessage(1, 42, 0, 0, OrderSide::Sell,
-                               MessageType::DeleteOrder));
+  book.deleteOrder(
+      makeMessage(1, 42, 0, 0, OrderSide::Sell, MessageType::DeleteOrder));
 
   const TopOfBook top = book.getTopOfBook();
 
@@ -106,13 +106,40 @@ TEST(OrderBookTest, DeleteOrderFallsBackToNextBestAsk) {
 TEST(OrderBookTest, ModifyOrderMovesBestBidToNewPrice) {
   OrderBook book(42);
 
-  book.addOrder(makeMessage(1, 42, 10, 100, OrderSide::Buy,
-                            MessageType::AddOrder));
-  book.modifyOrder(makeMessage(1, 42, 12, 75, OrderSide::Buy,
-                               MessageType::ModifyOrder));
+  book.addOrder(
+      makeMessage(1, 42, 10, 100, OrderSide::Buy, MessageType::AddOrder));
+  book.modifyOrder(
+      makeMessage(1, 42, 12, 75, OrderSide::Buy, MessageType::ModifyOrder));
 
   const TopOfBook top = book.getTopOfBook();
 
   EXPECT_EQ(top.bid_price, 12u);
   EXPECT_EQ(top.bid_qty, 75u);
+}
+
+TEST(OrderBookTest, BookUpdateAfterAddingOrders) {
+  OrderBook book(42);
+
+  book.addOrder(
+      makeMessage(1, 42, 10, 100, OrderSide::Buy, MessageType::AddOrder));
+  book.addOrder(
+      makeMessage(2, 42, 11, 50, OrderSide::Buy, MessageType::AddOrder));
+  book.addOrder(
+      makeMessage(3, 42, 12, 75, OrderSide::Sell, MessageType::AddOrder));
+  book.addOrder(
+      makeMessage(4, 42, 13, 25, OrderSide::Sell, MessageType::AddOrder));
+
+  const BookUpdate update = book.getBookUpdate();
+
+  EXPECT_EQ(update.symbol_id, 42u);
+  EXPECT_EQ(update.bids_depth, 2u);
+  EXPECT_EQ(update.asks_depth, 2u);
+  EXPECT_EQ(update.bids[0].price, 11u);
+  EXPECT_EQ(update.bids[0].qty, 50u);
+  EXPECT_EQ(update.bids[1].price, 10u);
+  EXPECT_EQ(update.bids[1].qty, 100u);
+  EXPECT_EQ(update.asks[0].price, 12u);
+  EXPECT_EQ(update.asks[0].qty, 75u);
+  EXPECT_EQ(update.asks[1].price, 13u);
+  EXPECT_EQ(update.asks[1].qty, 25u);
 }
